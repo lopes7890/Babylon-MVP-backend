aacc91e2ac9ab40fed80d5fe3a6de1e3cde0cb32
lopes7890 <lopesallison414@gmail.com>
2024-08-31 12:55:42 -0300

    correction BD

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,16 @@
 import { db } from "../db.js";
 
 export const rotaUsuario = async () => {
+//  const sql = "INSERT INTO usuario (nome, gmail, telefone, senha, idade, genero, idUsuario) VALUES (?, ?, ?, ?, ?, ?, ?)";
+//  const values = ['João', 'joao@example.com', '300974673', 'sebholpy', 16, 'masculino', 0];
   try{
+/*    db.query(sql, values, (err, result) => {
+      if (err) {
+        console.error('Erro ao inserir dados: ', err);
+        return;
+      }
+      console.log('Dados inseridos com sucesso! ID do novo registro: ', result.insertId);
+    });*/
     const [resultTableUsers] = await db.query('SELECT * FROM usuario');
     console.log('Resultados da consulta:', resultTableUsers);
     return resultTableUsers;
@@ -9,4 +18,5 @@ export const rotaUsuario = async () => {
       console.error('Erro na consulta:', err.stack);
       return;
   }
+  
 };
