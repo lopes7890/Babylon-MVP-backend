ed6ed39229318187211b6de6841087dbeb1495de
lopes7890 <lopesallison414@gmail.com>
2024-09-03 17:44:12 -0300

    Correction req

--- backend/routes/usuarios/usuariosPost.js
+++ backend/routes/usuarios/usuariosPost.js
@@ -1,14 +1,14 @@
 import { db } from "../../db.js";
 
 export const Postusuarios = async (req) => {
-    try{
-        const {nome, gmail, telefone, senha, idade, genero, idUsuario} = req.body;
-        await db.query('INSERT INTO usuario (nome, gmail, telefone, senha, idade, genero, idUsuario) VALUES (?, ?, ?, ?, ?, ?, ?)',
-        [nome, gmail, telefone, senha, idade, genero, idUsuario]
+      try{
+         const {nome, gmail, telefone, senha, idade, genero} = req.body;
+        await db.query('INSERT INTO usuario (nome, gmail, telefone, senha, idade, genero) VALUES (?, ?, ?, ?, ?, ?)',
+        [nome, gmail, telefone, senha, idade, genero]
         );
         return;
     } catch (err){
         console.log("erro");
         return;
-    }
+    } 
 }
