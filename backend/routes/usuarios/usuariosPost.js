c611dca5e8574e29f5275edf4deaa405834d6130
lopes7890 <lopesallison414@gmail.com>
2024-09-03 17:16:06 -0300

    register user in DB

--- backend/routes/usuarios/usuariosPost.js
+++ backend/routes/usuarios/usuariosPost.js
@@ -1,14 +1,30 @@
 import { db } from "../../db.js";
 
 export const Postusuarios = async (req) => {
-    try{
-        const {nome, gmail, telefone, senha, idade, genero, idUsuario} = req.body;
-        await db.query('INSERT INTO usuario (nome, gmail, telefone, senha, idade, genero, idUsuario) VALUES (?, ?, ?, ?, ?, ?, ?)',
-        [nome, gmail, telefone, senha, idade, genero, idUsuario]
+     try{
+        const q = 'INSERT INTO usuario (nome, gmail, telefone, senha, idade, genero) VALUES (?, ?, ?, ?, ?, ?)';
+        const values = [
+            req.body.nome, 
+            req.body.gmail, 
+            req.body.telefone, 
+            req.body.senha, 
+            req.body.idade,
+            req.body.genero]
+
+        await db.query(q, [values]);
+        return;
+    } catch (err){
+        console.log("erro");
+        return;
+    } 
+/*      try{
+         const {nome, gmail, telefone, senha, idade, genero, idUsuario} = req.body;
+        await db.query('INSERT INTO usuario (nome, gmail, telefone, senha, idade, genero) VALUES (?, ?, ?, ?, ?, ?)',
+        [nome, gmail, telefone, senha, idade, genero]
         );
         return;
     } catch (err){
         console.log("erro");
         return;
-    }
+    }   */
 }
